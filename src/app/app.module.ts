980c92ca5939a5ae3093166f418900551d98ed20
breno souza <90004546@pmenos.com.br>
2020-09-29 10:58:03 -0300

    Adding angular icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MatIconModule } from '@angular/material/icon';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,6 +9,10 @@ import { FormsModule } from '@angular/forms';
 import { NameEditorComponent } from './components/name-editor/name-editor.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ProfileEditorComponent } from './components/profile-editor/profile-editor.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { InputsModule, WavesModule, ButtonsModule } from 'angular-bootstrap-md';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NameEditorComponent, ProfileEditorComponent],
@@ -15,7 +20,14 @@ import { ProfileEditorComponent } from './components/profile-editor/profile-edit
     BrowserModule, 
     AppRoutingModule, 
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatSliderModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MDBBootstrapModule.forRoot()
+    // InputsModule,
+    // WavesModule,
+    // ButtonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/profile-editor/profile-editor.component.ts
+++ src/app/components/profile-editor/profile-editor.component.ts
@@ -11,7 +11,8 @@ export class ProfileEditorComponent implements OnInit {
 
   profileForm = new FormGroup({
     firstName: new FormControl('',Validators.required),
-    lastName: new FormControl(''),
+    lastName: new FormControl('',Validators.required),
+    email: new FormControl('', Validators.required),
     address: new FormGroup({
       street: new FormControl(''),
       city: new FormControl(''),
@@ -32,7 +33,7 @@ export class ProfileEditorComponent implements OnInit {
     console.warn(this.profileForm.value);
   }
   updateProfile(){
-    
+    console.log(this.profileForm.controls?.firstName?.touched);
     this.profileForm.patchValue({
       firstName: 'Nancy',
       address: {
